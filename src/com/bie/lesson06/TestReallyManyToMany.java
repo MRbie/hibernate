46e4c967fa4ef607cf6aad7ce5321513feb9ff14
MRbie <1748741328@qq.com>
2017-10-22 20:57:25 +0800

    hibernate的inverse控制反转对多对多映射的影响

--- src/com/bie/lesson06/TestReallyManyToMany.java
+++ src/com/bie/lesson06/TestReallyManyToMany.java
@@ -23,16 +23,16 @@ public class TestReallyManyToMany {
 		
 		//创建员工对象
 		Developer developer1 = new Developer();
-		developer1.setDeveloperName("张三");
+		developer1.setDeveloperName("张三3");
 		Developer developer2 = new Developer();
-		developer2.setDeveloperName("李四");
+		developer2.setDeveloperName("李四3");
 		
 		
 		//创建项目对象
 		Projects projects1 = new Projects();
-		projects1.setProjectName("OA系统");
+		projects1.setProjectName("OA系统3");
 		Projects projects2 = new Projects();
-		projects2.setProjectName("电商系统");
+		projects2.setProjectName("电商系统3");
 		
 		
 		//项目和员工之间的关系
@@ -50,6 +50,60 @@ public class TestReallyManyToMany {
 		//然后把项目关联的员工保存了
 		session.save(projects1);
 		session.save(projects2);
+		
+		//提交事务
+		tx.commit();
+		//关闭session
+		HibernateUtils.closeSession();
+		
+	}
+	
+	//设置inverse对获取数据的影响。
+	@Test
+	public void testSave(){
+		Session session = HibernateUtils.getSesion();
+		//开启事务
+		Transaction tx = session.beginTransaction();
+		
+		Projects projects = (Projects)session.get(Projects.class, 1);
+		System.out.println(projects.getProjectName());
+		System.out.println(projects.getDeveloper());
+		
+		//提交事务
+		tx.commit();
+		//关闭session
+		HibernateUtils.closeSession();
+		
+	}
+	
+	//设置inverse属性，对关联关系的影响。
+	@Test
+	public void testCancle(){
+		Session session = HibernateUtils.getSesion();
+		//开启事务
+		Transaction tx = session.beginTransaction();
+		
+		Projects projects = (Projects)session.get(Projects.class, 1);
+		//解除对应的关联关系
+		projects.getDeveloper().clear();
+		
+		
+		//提交事务
+		tx.commit();
+		//关闭session
+		HibernateUtils.closeSession();
+		
+	}
+	
+	//设置inverse属性，对删除关系的影响。
+	@Test
+	public void testDelete(){
+		Session session = HibernateUtils.getSesion();
+		//开启事务
+		Transaction tx = session.beginTransaction();
+		
+		Projects projects = (Projects)session.get(Projects.class, 1);
+		session.delete(projects);
 		
 		//提交事务
 		tx.commit();
