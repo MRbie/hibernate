dbccd78dfba0403827a863284671dd70b2f19d1d
MRbie <1748741328@qq.com>
2017-10-28 23:27:58 +0800

    hibernate继承映射每个实体类对应一张表

---
+++ src/com/bie/lesson10/extend03/Cat.java
@@ -0,0 +1,26 @@
+package com.bie.lesson10.extend03;
+/** 
+* @author  Author:别先生 
+* @date Date:2017年10月28日 下午9:27:04 
+*
+* 1:简单继承映射。
+* 	(1):总结，有多少子类，就写多少个映射配置文件。
+* 
+*/
+public class Cat extends Animal{
+
+	private String catchMouse;//猫的特性，抓老鼠
+
+	public String getCatchMouse() {
+		return catchMouse;
+	}
+	public void setCatchMouse(String catchMouse) {
+		this.catchMouse = catchMouse;
+	}
+	@Override
+	public String toString() {
+		return "Cat [catchMouse=" + catchMouse + "]";
+	}
+	
+	
+}
---
+++ src/com/bie/lesson10/extend03/Monkey.java
@@ -0,0 +1,23 @@
+package com.bie.lesson10.extend03;
+/** 
+* @author  Author:别先生 
+* @date Date:2017年10月28日 下午10:51:03 
+*
+*
+*/
+public class Monkey extends Animal{
+
+	private String eatBanana;
+
+	public String getEatBanana() {
+		return eatBanana;
+	}
+
+	public void setEatBanana(String eatBanana) {
+		this.eatBanana = eatBanana;
+	}
+
+	
+	
+	
+}
