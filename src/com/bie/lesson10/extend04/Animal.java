f131f0e986dea15389f09abc2885c485f8408938
MRbie <1748741328@qq.com>
2017-10-28 23:44:55 +0800

    hibernate的继承映射，推荐

---
+++ src/com/bie/lesson10/extend04/Animal.java
@@ -0,0 +1,27 @@
+package com.bie.lesson10.extend04;
+/** 
+* @author  Author:别先生 
+* @date Date:2017年10月28日 下午9:26:53 
+*
+*
+*/
+public class Animal {
+
+	private String animalId;//动物编号animalId animalName
+	private String animalName;//动物姓名
+	
+	public String getAnimalId() {
+		return animalId;
+	}
+	public void setAnimalId(String animalId) {
+		this.animalId = animalId;
+	}
+	public String getAnimalName() {
+		return animalName;
+	}
+	public void setAnimalName(String animalName) {
+		this.animalName = animalName;
+	}
+	
+	
+}
---
+++ src/com/bie/lesson10/extend04/ExtendTest.java
@@ -0,0 +1,45 @@
+package com.bie.lesson10.extend04;
+
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.junit.Test;
+
+import com.bie.lesson02.crud.utils.HibernateUtils;
+
+/** 
+* @author  Author:别先生 
+* @date Date:2017年10月28日 下午10:21:49 
+*
+* 
+*/
+public class ExtendTest {
+
+	@Test
+	public void test(){
+		Session session = HibernateUtils.getSesion();
+		Transaction tx = session.beginTransaction();
+		
+		//猫
+		Cat cat = new Cat();
+		cat.setAnimalName("大花猫");
+		cat.setCatchMouse("不抓老鼠.....");
+		
+		
+		//猴子
+		Monkey monkey = new Monkey();
+		monkey.setAnimalName("小猴子");
+		monkey.setEatBanana("吃大香蕉");
+		
+		//保存
+		session.save(cat);
+		session.save(monkey);
+		
+		//提交事务
+		tx.commit();
+		session.close();
+	}
+	
+}
